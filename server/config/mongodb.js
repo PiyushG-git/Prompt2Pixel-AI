a0363e99d3dfa68de3ab219610f793c9955fd5f2
PiyushG-git <piyushg251004@gmail.com>
2026-02-14 05:02:51 +0530

    feat: add backend structure with env safety

---
+++ server/config/mongodb.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    mongoose.connection.on("connected", () => {
+      console.log("Database Connected");
+    });
+
+    await mongoose.connect(
+      `${process.env.MONGODB_URI}/prompt2pixel_ai`
+    );
+  } catch (error) {
+    console.error("MongoDB connection failed:", error.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
---
+++ server/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+import connectDB from "./config/mongodb.js";
+
+console.log("Starting server file...");
+
+const PORT = process.env.PORT || 4000;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+await connectDB();
+
+app.get("/", (req, res) => {
+  res.send("API Working");
+});
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on PORT ${PORT}`);
+});
+
+
+// mongodb+srv://Prompt2PixelAI:Prompt2PixelAI123@cluster0.qez71h6.mongodb.net/?appName=Cluster0
