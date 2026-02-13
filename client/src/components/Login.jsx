c20f472aad9519331c54cfb344b24c29073022cc
PiyushG-git <piyushg251004@gmail.com>
2026-02-14 03:50:55 +0530

    add framer motion

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { assets } from '../assets/assets'
 import { AppContext } from '../context/AppContext';
+import { motion } from "motion/react";
 
 const Login = () => {
 
@@ -16,8 +17,13 @@ const Login = () => {
     },[])
 
   return (
-    <div className='absolute top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center'>  
-      <form className='relative bg-white p-10 rounded-xl text-slate-500'>
+    <motiodiv className='fixed top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center'
+    initial={{ opacity: 0.2, y: 50 }}
+      transition={{ duration: 0.3 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+    >  
+      <motion.form className='relative bg-white p-10 rounded-xl text-slate-500'>
         <h1 className='text-center text-2xl text-neutral-700 font-medium'>{state}</h1>
         <p className='text-sm'>Welcome back! Please sign in to continue</p>
 
@@ -47,8 +53,8 @@ const Login = () => {
         </p>}
 
         <img onClick={()=>{setShowLogin(false)}} src={assets.cross_icon} alt="" className='absolute top-5 right-5 cursor-pointer' />
-      </form>
-    </div>
+      </motion.form>
+    </motiodiv>
   )
 }
 
