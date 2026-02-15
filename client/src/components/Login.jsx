b5c0e95f6f9b05bde50d0aa3e53f6c89aa5326b2
PiyushG-git <piyushg251004@gmail.com>
2026-02-15 19:00:53 +0530

    connect the backend with frontend

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -2,12 +2,46 @@ import React, { useContext, useEffect, useState } from 'react'
 import { assets } from '../assets/assets'
 import { AppContext } from '../context/AppContext';
 import { motion } from "motion/react";
+import axios from 'axios'
+import { toast } from 'react-toastify';
 
 const Login = () => {
 
     const [state,setState] = useState('Login');
+    const {setShowLogin,backendUrl,setToken,setUser}=useContext(AppContext)
 
-    const {setShowLogin}=useContext(AppContext)
+    const [name,setName] = useState('')
+    const [email,setEmail] = useState('')
+    const [password,setPassword] = useState('')
+
+    const onSubmitHandler=async(e)=>{
+      e.preventDefault();
+      try {
+        if(state==='Login'){
+          const {data}= await axios.post(backendUrl+'/api/user/login',{email,password})
+          if(data.success){
+            setToken(data.token)
+            setUser(data.user)
+            localStorage.setItem('token', data.token)
+            setShowLogin(false)
+          }else{
+            toast.error(data.message)
+          }
+        } else{
+          const {data}= await axios.post(backendUrl+'/api/user/register',{name,email,password})
+          if(data.success){
+            setToken(data.token)
+            setUser(data.user)
+            localStorage.setItem('token', data.token)
+            setShowLogin(false)
+          }else{
+            toast.error(data.message)
+          }
+        }
+      } catch (error) {
+        toast.error(error.message)
+      }
+    }
 
     useEffect(()=>{
         document.body.style.overflow='hidden';
@@ -23,22 +57,22 @@ const Login = () => {
       whileInView={{ opacity: 1, y: 0 }}
       viewport={{ once: true }}
     >  
-      <motion.form className='relative bg-white p-10 rounded-xl text-slate-500'>
+      <motion.form onSubmit={onSubmitHandler} className='relative bg-white p-10 rounded-xl text-slate-500'>
         <h1 className='text-center text-2xl text-neutral-700 font-medium'>{state}</h1>
         <p className='text-sm'>Welcome back! Please sign in to continue</p>
 
         {state!=='Login' &&<div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-5'>
             <img src={assets.profile_icon} alt="" width={25}/>
-            <input type="text" className='outline-none text-sm' placeholder='Full Name' required />
+            <input onChange={e=>setName(e.target.value)} value={name} type="text" className='outline-none text-sm' placeholder='Full Name' required />
         </div>}
 
         <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-4'>
             <img src={assets.email_icon} alt=""/>
-            <input type="email" className='outline-none text-sm' placeholder='Email id' required />
+            <input onChange={e=>setEmail(e.target.value)} value={email} type="email" className='outline-none text-sm' placeholder='Email id' required />
         </div>
         <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-4'>
             <img src={assets.lock_icon} alt="" width={25}/>
-            <input type="password" className='outline-none text-sm' placeholder='Password' required />
+            <input onChange={e=>setPassword(e.target.value)} value={password} type="password" className='outline-none text-sm' placeholder='Password' required />
         </div>
 
         <p className='text-sm text-blue-600 my-4 cursor-pointer'>Forgot password</p>
--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { assets } from "../assets/assets";
 import { motion } from "motion/react";
+import { useContext } from "react";
+import { AppContext } from "../context/AppContext";
 
 const Result = () => {
   const [image, setImage] = useState(assets.sample_img_1);
@@ -8,8 +10,19 @@ const Result = () => {
   const [loading,setLoading] = useState(false);
   const [input,setInput]= useState("");
 
+  const {generateImage} = useContext(AppContext)
+
   const onSubmitHandler = async(e)=>{
-    
+    e.preventDefault()
+    setLoading(true)
+    if(input){
+      const image=await generateImage(input)
+      if(image){
+        setIsImageLoaded(true)
+        setImage(image)
+      }
+    }
+    setLoading(false)
   }
 
   return (
@@ -21,7 +34,7 @@ const Result = () => {
     >
       <div>
         <div className="relative">
-          <img src={assets.sample_img_1} alt="" className="max-w-sm rounded" />
+          <img src={image} alt="" className="max-w-sm rounded" />
           <span className={`absolute bottom-0 left-0 h-1 bg-blue-500 ${loading?'w-full transition-all duration-[10s]':'w-0'}`} />
         </div>
         <p className={!loading?'hidden':''}>loading.......</p>
